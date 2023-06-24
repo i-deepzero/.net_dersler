edbd9849dcc065e7ed72778da0b3ae05ea7ae8d2
İsmail Yusuf Gürsul <104919360+i-deepzero@users.noreply.github.com>
2023-06-24 15:18:44 +0300

    ders6

---
+++ Ders_6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Ders_6
+{
+        class Program{
+        public static void Main(string[] args){
+            
+            int time=DateTime.Now.Hour;
+            if (time < 8 && time >6){
+
+                    Console.WriteLine("Good Morning!");
+
+            }else if (time >= 9 && time<=22){
+
+                Console.WriteLine("Good Day");
+
+            }else{
+                Console.WriteLine("Good Nigh");
+
+            }
+            String fc = time >8 ?"good morning":"good night";//burda koşul kontrol ettiriyoz ilki sağlamıyosa ikincisi sağlanmıyosa
+
+            fc = time>= 6 && time <=7 ?"günaydın":time<=18?"iyi akşamlar":"iyigeceler";
+             
+            Console.WriteLine(fc);
+            Console.ReadKey();
+
+            
+        }
+    }
+}
